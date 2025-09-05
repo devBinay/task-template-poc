b99889ca1efd2844f5bd0e3832de7b3e0c556628
Gyaneshwar Kumar <gyaneshwar.kumar@logile.com>
2025-09-05 21:15:04 +0530

    added more icons

--- src/core/constants/Icons.tsx
+++ src/core/constants/Icons.tsx
@@ -1,4 +1,12 @@
-import SearchIcon from "../../assets/icons/search.svg?react";
+import SearchIcon from "../../assets/images/icons/search.svg?react";
+import CheckIcon from "../../assets/images/icons/check.svg?react";
+import CloseIcon from "../../assets/images/icons/close.svg?react";
+import CopyIcon from "../../assets/images/icons/copy.svg?react";
+import ThumbsUp from "../../assets/images/icons/thumbs-up.svg?react";
 export const icons =  { 
     search: SearchIcon,
+    check: CheckIcon,
+    close: CloseIcon,
+    copy: CopyIcon,
+    thumbsUp: ThumbsUp
 };
