c09b55f9c01229839bc5d5785c43d2255b4c4d87
bhupendrashekhawat9 <87308578+bhupendrashekhawat9@users.noreply.github.com>
2025-09-18 15:43:27 +0000

    add: base color

--- src/pages/TemplateLibrary/index.tsx
+++ src/pages/TemplateLibrary/index.tsx
@@ -123,7 +123,7 @@ const TemplateLibrary: React.FC = () => {
         <Stack direction={"row"} alignItems={'center'}>
           <IconButton variant="outline" disableHover={true} disableTouchRipple sx={{
                marginRight:'var(--space-lg)',
-               backgroundColor:'var(--bg-default)',
+               backgroundColor:'var(--bg-Container-1)',
                padding:'.8rem'
           }}>
           <SvgIcon component={"chevronLeft"} fill='var(--icon-color-secondary)' size={18}/>
